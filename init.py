d99a725584052aa57a3a16fec24e793ddfa078ba
Andreas Smedemark Hammer <au606887@uni.au.dk>
2024-11-21 11:03:37 +0100

    cleaned up init.py, and added a test of workflow

--- init.py
+++ init.py
@@ -3,9 +3,12 @@ import os
 
 argparser=argparse.ArgumentParser(description="Initiator to rename files to fit the day of the task.")
 
-argparser.add_argument(--day, type=str, required=True)
+argparser.add_argument('day', type=str, required=True)
 args = argparser.parse_args()
 
+def test_dir(srcPath, trgPath):
+    assert os.path.exists(srcPath)
+    assert os.path.exists(trgPath)
 
 if __name__=="__main__":
     args = argparser.parse_args()
@@ -14,4 +17,8 @@ if __name__=="__main__":
     test_file = os.path.join(base_path, f"test/test_{args.day}.py")
     main_file = os.path.join(base_path, f"main/{args.day}.py")
 
-    os.rename()
+    test_dir(test_file, main_file)
+    
+    #os.rename()
+
+
